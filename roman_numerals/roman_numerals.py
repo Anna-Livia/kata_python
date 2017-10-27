50407ba522c9ce76c244e507449b29b6bc20b81e
Anna-Livia <annalivia.cardin@gmail.com>
2017-10-27 16:30:25 +0200

    Make it work from 0 to 5

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,2 +1,13 @@
 def loaded():
     return True
+
+
+def to_roman_numeral(number):
+    if number <= 3:
+        return number * "I"
+    elif number <= 5:
+        if number < 5:
+            return "IV"
+        else:
+            return "V"
+
--- roman_numerals/test_roman_numerals.py
+++ roman_numerals/test_roman_numerals.py
@@ -1,6 +1,22 @@
 from nose.tools import assert_equals, assert_almost_equal
-from roman_numerals import loaded
+from roman_numerals import loaded, to_roman_numeral
 
 
 def test_loaded():
     assert loaded() == True
+
+
+def test_1_gives_I():
+    assert_equals(to_roman_numeral(1), "I")
+
+
+def test_2_gives_II():
+    assert_equals(to_roman_numeral(2), "II")
+
+
+def test_4_gives_IV():
+    assert_equals(to_roman_numeral(4), "IV")
+
+
+def test_5_gives_V():
+    assert_equals(to_roman_numeral(5), "V")
